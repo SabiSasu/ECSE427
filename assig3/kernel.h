afddc49a0080f0cd68fb487395f768b42435afe2
sabina <sabina.sasu@gmail.com>
2020-03-19 13:29:12 -0400

    assig3 stuff

---
+++ assig3/kernel.h
@@ -0,0 +1,5 @@
+int myinit(char *filename);
+char *fileNameExec;
+void addToReady(struct PCB *pcb);
+void printQueue();
+int scheduler();
---
+++ assig3/ram.h
@@ -0,0 +1,10 @@
+extern char *ram[1000];
+
+char * getRAM(int k);
+void initiateRAM();
+void loadPageRAM(int frame, char *fInput);
+int addToRAM(FILE *p, int *start, int *end);
+void cleanUpRAM();
+void printRAM();
+void printAllRAM();
+int getNextFrame();
